858db5c9ac334ba079a76d1ce797938772030829
SIXIVESS <mettatonbb@gmail.com>
2024-02-29 16:20:11 -0700

    arreglo

--- app/src/main/java/mx/itson/edu/practica6/MainActivity.kt
+++ app/src/main/java/mx/itson/edu/practica6/MainActivity.kt
@@ -1,46 +1,21 @@
+
 package mx.itson.edu.practica6
 
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import mx.itson.edu.practica6.ui.theme.Practica6Theme
+import android.widget.Button
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Practica6Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        val button : Button = findViewById(R.id.botonP)
+
+        button.setOnClickListener(){
+            var intent: Intent = Intent(this, catalogo::class.java)
+            startActivity(intent)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Practica6Theme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/mx/itson/edu/practica6/Pelicula.kt
@@ -0,0 +1,6 @@
+package mx.itson.edu.practica6
+
+data class Pelicula(var titulo:String,
+                    var image:Int,
+                    var header:Int,
+                    var sinopsis:String)
